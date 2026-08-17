dc978d8300b27b6970dc4a176aa240b8b3d313c0
Simran Kaur <simrankaur200620@gmail.com>
2026-08-17 14:55:46 +0530

    graph practice

---
+++ Graphs/BFS.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+void bfs(int start, vector<vector<int>> &adj, int V) {
+    vector<bool> visited(V, false);
+    queue<int> q;
+
+    visited[start] = true;
+    q.push(start);
+
+    while (!q.empty()) {
+        int node = q.front();
+        q.pop();
+
+        cout << node << " ";
+
+        for (int neighbor : adj[node]) {
+            if (!visited[neighbor]) {
+                visited[neighbor] = true;
+                q.push(neighbor);
+            }
+        }
+    }
+}
+
+int main() {
+    int V, E;
+    cin >> V >> E;
+
+    vector<vector<int>> adj(V);
+
+    for (int i = 0; i < E; i++) {
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u); // Remove this line for a directed graph
+    }
+
+    int start;
+    cin >> start;
+
+    bfs(start, adj, V);
+
+    return 0;
+}
